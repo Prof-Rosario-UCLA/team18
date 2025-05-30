200efc2e9f07b95bc3b05af2f5769f9b53d7b1c5
Emily Yu <emilyyu331@gmail.com>
2025-05-30 00:54:10 -0700

    spin animation for dog icon for fun

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTheme } from "@/context/theme-provider";
 import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
@@ -6,31 +7,51 @@ const Header = () => {
   const { theme, setTheme } = useTheme();
   const isDark = theme === "dark";
 
-  return ( 
-  <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
-    <div className="container mx-auto flex h-16 items-center justify-between px-4">
-      <Link to={"/"}>
-        <img src="/sun_logo.png" alt="Weather Tracker Logo" className="h-30"/>
-      </Link>
+  const [spinning, setSpinning] = useState(false);
 
-      <div>
-        {/* search */}
-        {/* theme toggle */}
-        <div onClick={() => setTheme(isDark ? "light" : "dark")}
-          className={`flex items-center cursor-pointer transition-transform duration-500 ${
-            isDark ? "rotate-180" : "rotate-0"
-          }`}
-        >
-          {isDark? (
-            <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all"/>
-          ):(
-            <Moon className="h-6 w-6 text-black-500 rotate-0 transition-all"/>
-          )}
+  // Handler to make spin animation
+  const handleSpin = () => {
+    setSpinning(true);
+  };
+
+  // Remove after animation end
+  const handleAnimationEnd = () => {
+    setSpinning(false);
+  };
+
+  return ( 
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <Link to={"/"}>
+          <img 
+            src="/sun_logo.png" 
+            alt="Weather Tracker Logo" 
+            className={`h-30 ${spinning ? "spin" : ""}`} 
+            onClick={handleSpin} 
+            onAnimationEnd={handleAnimationEnd}
+            style={{ cursor: "pointer" }}
+          />
+        </Link>
+
+        <div>
+          {/* search */}
+          {/* theme toggle */}
+          <div 
+            onClick={() => setTheme(isDark ? "light" : "dark")}
+            className={`flex items-center cursor-pointer transition-transform duration-500 ${
+              isDark ? "rotate-180" : "rotate-0"
+            }`}
+          >
+            {isDark ? (
+              <Sun className="h-6 w-6 text-yellow-500 rotate-0 transition-all"/>
+            ) : (
+              <Moon className="h-6 w-6 text-black-500 rotate-0 transition-all"/>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </header>
   );
 };
 
-export default Header
+export default Header;
